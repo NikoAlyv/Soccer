c9c4c61c74e3b5cec024512ad1d6c63ae577e95f
NikoAlyv <enihad801@gmail.com>
2025-03-19 06:54:40 +0400

    feat:title updated

--- Soccer/ContentView.swift
+++ Soccer/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
+    @State var selected = "Baseball"
+    @State private var id = 1
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack{
+            Text("Selected Activity...")
+                    .font(.largeTitle.bold())
+            Spacer()
+
+            Circle()
+                .fill(colors.randomElement() ?? .blue).padding() .overlay(
+                Image(systemName: "figure.\(selected.lowercased())")
+            )  .font(.system(size: 144))
+                .foregroundColor(.white);
+            Text("\(selected)!")
+                .font(.title)
+            
+            Spacer()
+
+            Button("Try again") {
+                withAnimation(.easeInOut(duration: 1)) {
+                      selected = activities.randomElement() ?? "Archery"
+                    id += 2
+                    
+
+                  }
+                
+
+            }
+            .buttonStyle(.borderedProminent)
+        }.transition(.slide)
+            .id(id)
     }
 }
 
