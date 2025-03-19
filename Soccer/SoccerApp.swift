f86f108c305102db6ed353e33dde151387a1925b
Nihad <enihad801@gmail.com>
2025-03-19 05:46:48 +0400

    Initial Commit

---
+++ Soccer/SoccerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerApp.swift
+//  Soccer
+//
+//  Created by Nihad  on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SoccerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoccerTests/SoccerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerTests.swift
+//  SoccerTests
+//
+//  Created by Nihad  on 19/03/2025.
+//
+
+import Testing
+@testable import Soccer
+
+struct SoccerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
